5a41ef306838a40cf22e16677f45d9efae5e2e29
Gabriel Loyola <gabrielfrml@gmail.com>
2023-03-20 11:18:32 -0300

    fix: migration survivors with required fields

--- db/migrate/20230320044252_create_survivors.rb
+++ db/migrate/20230320044252_create_survivors.rb
@@ -1,9 +1,9 @@
 class CreateSurvivors < ActiveRecord::Migration[6.1]
   def change
     create_table :survivors do |t|
-      t.string :name
-      t.integer :age
-      t.string :gender
+      t.string :name, null: false
+      t.integer :age, null: false
+      t.string :gender, null: false
       t.float :last_lat
       t.float :last_long
 
